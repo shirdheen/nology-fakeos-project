e55d5d151aca027cc44c651d0dfda63fdf28b9f5
shirdheen <shirdheen2252@gmail.com>
2025-01-03 18:26:57 +1100

    Weather app modal done

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+import { handleWeatherRequest } from "./modules/weatherUI-handler.js";
 import { updateTime } from "./modules/updateTime.js";
 
-// Call the updateTime function to start the clock
 updateTime();
+
+document.getElementById("weatherForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleWeatherRequest();
+});
+
+document.querySelector(".icon-container").addEventListener("click", () => {
+  document.getElementById("weatherModal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", () => {
+  document.getElementById("weatherModal").style.display = "none";
+
+  weatherForm.reset();
+  weatherData.style.display = "none";
+});
